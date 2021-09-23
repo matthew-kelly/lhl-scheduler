02c5b9ecc09abf029b1475ba61da1904a46d79c6
Matt Kelly <bmatthewkelly@gmail.com>
2021-09-23 13:15:48 -0700

    add getInterview helper function

--- src/helpers/selectors.js
+++ src/helpers/selectors.js
@@ -8,3 +8,11 @@ export function getAppointmentsForDay(state, day) {
   );
   return appointments;
 }
+
+export function getInterview(state, interview) {
+  // replace interview.interviewer with info from state
+  if (!interview) return null;
+  const interviewer = state.interviewers[interview.interviewer];
+  if (!interviewer) return null;
+  return { ...interview, interviewer };
+}
