fb12b47835b841c7807378b6afbdab4c68719a88
Matt Kelly <bmatthewkelly@gmail.com>
2021-09-28 13:41:06 -0700

    remove console.log

--- src/hooks/useApplicationData.js
+++ src/hooks/useApplicationData.js
@@ -17,7 +17,6 @@ export function useApplicationData() {
       axios.get(`/api/interviewers`),
     ])
       .then((all) => {
-        console.log({ days: all[0].data, appointments: all[1].data });
         setState((prev) => ({
           ...prev,
           days: all[0].data,
